c6c2d7f0ef996227f4ac72fa83984b7062be96fb
Dmitrii Damyan <dmitriignc@gmail.com>
2025-04-08 15:38:12 +0300

    solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,12 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  if (items.length === 0) {
+    return {};
+  }
+
+  const result: object = [...items];
+
+  return result.filter((item) => item.includs(item.key));
 }
